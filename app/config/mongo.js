61c09f3e4f4616275181497f490f1757d0b78ad6
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-03-05 16:42:09 +0400

    added mongo configs

--- app/config/mongo.js
+++ app/config/mongo.js
@@ -4,7 +4,40 @@
 exports.default = function (app) {
     return {
         mongo: {
-            uri: 'mongodb://192.168.1.183/f-storage'
+            uri: 'mongodb://192.168.1.183/f-storage-development'
+        }
+    }
+};
+
+/**
+ *  NODE_ENV = development
+ */
+exports.development = function (app) {
+    return {
+        mongo: {
+            uri: 'mongodb://192.168.1.183/f-storage-development'
+        }
+    }
+};
+
+/**
+ *  NODE_ENV = staging
+ */
+exports.staging = function (app) {
+    return {
+        mongo: {
+            uri: 'mongodb://192.168.1.183/f-storage-staging'
+        }
+    }
+};
+
+/**
+ *  NODE_ENV = production
+ */
+exports.production = function (app) {
+    return {
+        mongo: {
+            uri: 'mongodb://192.168.1.183/f-storage-production'
         }
     }
 };
