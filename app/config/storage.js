e8a319fb7a62c2bd6ac5e6f335ae7441b28fed99
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-03-07 17:37:43 +0400

    fix

--- app/config/storage.js
+++ app/config/storage.js
@@ -5,7 +5,7 @@ exports.default = function (app) {
     return {
         storage: {
             maxFileSize: 10000000, // bytes (10 000 000 bytes === 1 mb)
-            tempDir: './tempFiles/'
+            tempDir: 'tempFiles/'
         }
     }
 };
